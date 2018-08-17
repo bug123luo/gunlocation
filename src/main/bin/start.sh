be360cd7562fcb6df73b0d7a89d810d27f8b5de8
bug123luo <luo_3050120123@163.com>
2018-08-17 18:05:09 +0800

    将pom修改成方便部署生成zip方式   需要在eclipse中的Run->Maven build...配置设置gooal为 clean assembly:assembly 执行Maven build命令

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -7,16 +7,16 @@ fi
  
 bin=`dirname "$1"`
  
-export MYJETTY_HOME=`cd $bin/../; pwd`
+export GUN_HOME=`cd $bin/../; pwd`
  
-MYJETTY_CONF_DIR=$MYJETTY_HOME/conf
-CLASSPATH="${MYJETTY_CONF_DIR}"
+GUN_CONF_DIR=$MYJETTY_HOME/conf
+CLASSPATH="${GUN_CONF_DIR}"
  
-for f in $MYJETTY_HOME/lib/*.jar; do
+for f in $GUN_HOME/lib/*.jar; do
   CLASSPATH=${CLASSPATH}:$f;
 done
  
-LOG_DIR=${MYJETTY_HOME}/logs
+LOG_DIR=${GUN_HOME}/logs
  
 CLASS=com.tct.server.GunLocationMQServer
-nohup ${JAVA_HOME}/bin/java -classpath "$CLASSPATH" $CLASS > ${LOG_DIR}/myjetty.out 2>&1 < /dev/null &
+nohup ${JAVA_HOME}/bin/java -classpath "$CLASSPATH" $CLASS > ${LOG_DIR}/gunlocation.log 2>&1 < /dev/null &
